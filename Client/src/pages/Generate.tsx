af711467cebc46413d63dd814f50f3416e57f978
geettalati <2340401112@stu.manit.ac.in>
2026-01-06 23:35:45 +0530

    `Updated login form and soft backdrop components with color scheme changes`

--- Client/src/pages/Generate.tsx
+++ Client/src/pages/Generate.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
+import { useParams } from "react-router-dom"
+import type { IThumbnail } from "../assets/assets"
+import Softbackdrop from "../components/Softbackdrop"
 
 
 const Generate = () => {
+
+    const {id} = useParams()
+    const [title,setTitle] = useState('')
+    const [additionaldetails,setadditionaldetails] = useState('')
+
+    const [thumbnail ,setThumbnail] = useState<IThumbnail | null>(null)
+    const [loading ,setloading] =useState(false)
+  
+
   return (
     <div>
+      <Softbackdrop />
+      
       
     </div>
   )
