c0cdf1fdd79a2672f009daf82d23e56ce079361e
geettalati <2340401112@stu.manit.ac.in>
2026-01-12 23:12:09 +0530

    `Updated dependencies and added MongoDB connection configuration`

---
+++ Server/models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password?: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const userschema = new mongoose.Schema<IUser>({
+    name:{
+        type:String,
+        required:true,
+        trim:true,   
+    },
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        unique:true,
+        lowercase:true,
+    },
+
+},{timestamps:true})
+
+const user = mongoose.models.user || mongoose.model<IUser>('user' , userschema)
+
+export default user;
--- Server/server.ts
+++ Server/server.ts
@@ -1,17 +1,53 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+declare module 'express-session' {
+  interface SessionData {
+    isLoggedIn?: boolean;
+    userId?: string;
+  }
+}
+
 const app = express();
 
-app.use(cors())
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials: true,
+  })
+);
 
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use(express.json())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'dev-secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24**7, // 7 days
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    },
+  })
+);
+
+const port = Number(process.env.PORT) || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+  res.send('Server is Live!');
 });
 
-app.listen(port, () => {
+const startServer = async () => {
+  await connectDB();
+  app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
-});
+  });
+};
+
+startServer();
